218d8bbfecfdfd8bfe63c8f33d2fe9ef418c7caf
om <70848538+Quints497@users.noreply.github.com>
2021-08-20 17:50:48 +0100

    Stores the dealers cards

---
+++ Characters/Blackjack/bjdealer.py
@@ -0,0 +1,22 @@
+"""
+Author: Oscar Miles\n
+Date: 20/8/21
+"""
+
+class BjDealer:
+    """
+    Used to store the information about the blackjack dealer
+
+    ...
+
+    Attributes
+    ----------
+    username : str
+        The dealers name - "Dealer"
+    cards : list
+        The dealers cards
+    """
+    def __init__(self):
+        self.username = "Dealer"
+        self.cards = []
+
